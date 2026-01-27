005ce32f0e228b5d2657a61df7dfeff433000806
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2026-01-27 11:35:29 +0100

    premier commit

---
+++ src/test/java/outbusinessproject/outbusinessproject/OutBusinessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package outbusinessproject.outbusinessproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutBusinessProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
