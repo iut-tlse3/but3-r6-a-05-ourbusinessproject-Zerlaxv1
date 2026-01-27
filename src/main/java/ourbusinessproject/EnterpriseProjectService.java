7995406e7ce42ca568c56d080cd01ca0ef2ae951
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2026-01-27 16:32:28 +0100

    fix #1.4 classe EnterpriseProjectService

---
+++ src/main/java/ourbusinessproject/EnterpriseProjectService.java
@@ -0,0 +1,52 @@
+package ourbusinessproject;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EnterpriseProjectService {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    public EnterpriseProjectService(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public Project findProjectById(Long projectId) {
+        return entityManager.find(Project.class, projectId);
+    }
+
+    public Enterprise findEnterpriseById(Long entrepriseId) {
+        return entityManager.find(Enterprise.class, entrepriseId);
+    }
+
+    public Enterprise newEnterprise(String aName, String aDescription, String aContactName, String mail) {
+        Enterprise enterprise = new Enterprise();
+        enterprise.setName(aName);
+        enterprise.setDescription(aDescription);
+        enterprise.setContactName(aContactName);
+        enterprise.setContactEmail(mail);
+
+        entityManager.persist(enterprise);
+        entityManager.flush();
+
+        return enterprise;
+    }
+
+    public Project newProject(String aTitle, String aDescription) {
+        Project project = new Project();
+
+        project.setTitle(aTitle);
+        project.setDescription(aDescription);
+
+        entityManager.persist(project);
+        entityManager.flush();
+        return project;
+    }
+}
