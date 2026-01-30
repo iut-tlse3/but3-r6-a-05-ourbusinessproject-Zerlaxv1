bd41ea328991a31f6b670d0b8fcc91721d31d1c0
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2026-01-30 09:52:25 +0100

    remove unused imports

--- src/main/java/ourbusinessproject/ProjectController.java
+++ src/main/java/ourbusinessproject/ProjectController.java
@@ -1,7 +1,5 @@
 package ourbusinessproject;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
