e567240b12f4eb145e19856859f8f34f2ef80fc0
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2026-01-30 11:03:39 +0100

    fix #4.1 classe Partnership

--- src/main/java/ourbusinessproject/Partnership.java
+++ src/main/java/ourbusinessproject/Partnership.java
@@ -1,4 +1,44 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+
+@Entity
 public class Partnership {
+
+    @NotNull
+    Date creationDate;
+
+    @NotNull
+    Enterprise enterprise;
+
+    @NotNull
+    Project project;
+
+    public Project getProject() {
+        return project;
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
 }
